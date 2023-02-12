677b60db05b4dfd40e1ec6171cac4d9b68eccc82
Yasir Khan <yasirkhanone22@gmail.com>
2023-02-12 23:50:32 +0500

    Narrowing Explained

---
+++ dist/Narrowing.js
@@ -0,0 +1,10 @@
+"use strict";
+// type narrowing goes here.
+// let x: number | string;
+// x = 34;
+// if (typeof x === "number") {
+//     console.log(x.toFixed(2)); // ok
+//     x = "Hello"; // here x is now assigned to string.
+// }
+// console.log(x.toFixed(3)) // number methods are assignable to string.
+//# sourceMappingURL=Narrowing.js.map
