4fe92d9013ffbf24849148ec2c9cfaa4683b6941
yasir-khan22 <yasirkhanone22@gmail.com>
2023-07-10 22:00:27 +0500

    Tuple explained.

---
+++ dist/TypescriptTypes/TupleExplained.js
@@ -0,0 +1,12 @@
+"use strict";
+/**
+ * A tuple is typed array with pre-defined length and types for each index.
+ * Each element in tuple is known type of value.
+ * Tuple has fixed number of elements where each element has it's own types.
+ */
+/**
+ *! for example.
+ */
+let person = ["John", 23, true];
+console.log(person);
+//# sourceMappingURL=TupleExplained.js.map
