8c272489e711fa1128cba659de6daea5b671666d
yasir-khan22 <yasirkhanone22@gmail.com>
2023-07-19 00:24:55 +0500

    functions defined.

---
+++ src/TypescriptTypes/Functions.ts
@@ -0,0 +1,14 @@
+// functions are core building block of typescript.
+// function in typescript can be declared by function declaration syntax or by function expressions syntax.
+
+// function declaration.
+function addition(a: string, b: string): string {
+    return a + b;
+}
+
+// function expression.
+function subtraction(a: number, b: number): number {
+    return a - b
+}
+const subtract = subtraction(2, 4);
+console.log(subtract)
