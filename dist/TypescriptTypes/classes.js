752f14d7d7120cae562391cd3f08b7ca7d8f4894
yasir-khan22 <yasirkhanone22@gmail.com>
2023-07-31 22:25:10 +0500

    classes again covered.

--- dist/TypescriptTypes/classes.js
+++ dist/TypescriptTypes/classes.js
@@ -1,36 +1,51 @@
 "use strict";
 // Classes
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Message = void 0;
 /**
  * Classes in typescript are blue-prints for creating objects which contains methods and properties.
  * It helps in organizing code, promoting code useability and implementing object oriented (OOP) concepts.
  */
-class Animal {
-    constructor(name, age) {
-        this.name = name;
-        this.age = age;
-    }
-    makeSound() {
-        console.log("Animal makes a sound.");
+// class Animal {
+//     name: string;
+//     age: string;
+//     constructor(name: string, age: string) {
+//         this.name = name;
+//         this.age = age;
+//     }
+//     makeSound() {
+//         console.log("Animal makes a sound.")
+//     }
+// }
+// const dog = new Animal("librador", "33",);
+// console.log(dog)
+// console.log(dog.name)
+// console.log(dog.age)
+// console.log(dog.makeSound())
+// class Dog extends Animal {
+//     breed: string;
+//     constructor(name: string, age: string, breed: string) {
+//         super(name, age);
+//         this.breed = breed;
+//     }
+//     makeSound(): void {
+//         console.log("Dogs also makes sound.")
+//     }
+// }
+// const DogOne = new Dog("Jimmy", "33", "doberman");
+// const DogTwo = new Dog("Fluffy", "22", "librador")
+// console.log(DogOne);
+// console.log(DogOne.name);
+// console.log(DogOne.age);
+// console.log(DogOne.makeSound());
+class Message {
+    constructor(title, message) {
+        this.title = title;
+        this.message = message;
+        this.isSent = false;
     }
 }
-const dog = new Animal("librador", "33");
-console.log(dog);
-console.log(dog.name);
-console.log(dog.age);
-console.log(dog.makeSound());
-class Dog extends Animal {
-    constructor(name, age, breed) {
-        super(name, age);
-        this.breed = breed;
-    }
-    makeSound() {
-        console.log("Dogs also makes sound.");
-    }
-}
-const DogOne = new Dog("Jimmy", "33", "doberman");
-const DogTwo = new Dog("Fluffy", "22", "librador");
-console.log(DogOne);
-console.log(DogOne.name);
-console.log(DogOne.age);
-console.log(DogOne.makeSound());
+exports.Message = Message;
+const newOne = new Message("Hello", "world");
+const secondOne = new Message("Hi!", "buddy");
 //# sourceMappingURL=classes.js.map
