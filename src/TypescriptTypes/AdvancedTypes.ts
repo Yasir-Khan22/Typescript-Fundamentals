0a430a83dafc91c730521cd4c2e389318f89e2cc
yasir-khan22 <yasirkhanone22@gmail.com>
2023-08-09 23:42:08 +0500

    :Explaination advanced types.

--- src/TypescriptTypes/AdvancedTypes.ts
+++ src/TypescriptTypes/AdvancedTypes.ts
@@ -7,3 +7,22 @@
  * 4. Template Literal Types.
  * 5. Recursise Types.
  */
+
+
+/**
+* 1. Mappped types are use a similar syntax to loops, where you iterate over the keys of an existing type and apply a transformation to each property.
+ */
+
+type ReadOnly<T> = {
+    readonly [P in keyof T]: T[P];
+}
+
+interface Person {
+    name: string;
+    age: string
+}
+
+const readOnlyPerson: Readonly<Person> = {
+    name: "Yasir",
+    age: "22"
+}
